41b5634db7da63bd90291cf1c1ccd797c58e40e6
Glenn Wood <glenn@hazelst.com>
2018-05-30 18:25:24 -0400

    Miscellaneous commit; handy scripts for later, etc.

--- install/os-imaging.sh
+++ install/os-imaging.sh
@@ -1,7 +1,85 @@
 #/bin/bash
 
-### NOW CLONE THIS SO WE DON'T HAVE TO DO THIS OVER AND OVER AANNDOVER OASCO OVER AND OVER AND OVER!!!!!!!
+    if [[ $EUID -ne 0 ]]
+    then
+		echo "$0 must be run as root."
+		sudo -H $0 $*
+		exit $?
+	fi
+
+### Ref: http://releases.ubuntu.com/16.04/
+###      http://releases.ubuntu.com/16.04/ubuntu-16.04.4-server-i386.iso
+### Ref: http://releases.ubuntu.com/18.04/
+###      
+
+###
+### To create bootable USB from currently running Ubuntu.
+### Ref: https://answers.launchpad.net/systemback/+question/273061#5
+=============================================== systemback
+### Generate sblive image of currently running Ubuntu.
+add-apt-repository -y ppa:nemh/systemback
+apt-get update
+apt-get -y install systemback unionfs-fuse genisoimage
+
+# AS USER $MINERS_USER
+sudo systemback
+
+# To generate the ISO file:
+
+1. Decompress the .sblive image:
+mkdir sblive
+tar -xf /home/{ the-name-you-gave-it}.sblive -C sblive
+
+2. Rename the syslinux to isolinux:
+mv sblive/syslinux/syslinux.cfg sblive/syslinux/isolinux.cfg
+mv sblive/syslinux sblive/isolinux
+
+3. Generate the ISO file:
+genisoimage -iso-level 3 -r -V sblive -cache-inodes -J -l -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -c isolinux/boot.cat -o mining-20180520.iso sblive
+
+add-apt-repository -y ppa:gezakovacs/ppa
+apt-get update ; apt-get -y install unetbootin
+
+## mount -t vfat /dev/sda2 /media/usb -o uid=1000,gid=100,utf8,dmask=027,fmask=137
+## mount -t ntfs-3g /dev/sda2 /media/usb
+## neither of those worked, so I just copied the ISO to my MacBook and ran Unetbootin from there.
+
+
+
+### CLONING AN SSD SO WE DON'T HAVE TO DO THIS OVER AND OVER AANNDOVER OASCO OVER AND OVER AND OVER!!!!!!!
 ### https://www.makeuseof.com/tag/2-methods-to-clone-your-linux-hard-drive/
 
 echo 'This will take about eight (8) minutes'
 dd if=/dev/sda of=/dev/sdb bs=64K conv=noerror,sync
+
+
+### OR
+add-apt-repository -y ppa:nemh/systemback
+apt-get update
+apt-get -y install systemback unionfs-fuse
+
+# AS USER $MINERS_USER
+sudo systemback
+
+
+### Ref: https://help.ubuntu.com/community/mkusb
+### 'mkusb - dd image of iso file to USB device safely'
+add-apt-repository -y universe
+add-apt-repository -y ppa:mkusb/ppa
+apt-get update
+apt-get -y install mkusb mkusb-nox usb-pack-efi
+
+### Ref: http://phillw.net/isos/linux-tools/mkusb/mkUSB-quick-start-manual.pdf
+mkusb /home/miners-2018-05-22.iso
+
+
+### Clean up logs
+cd $MINING_ROOT/install/log
+sudo su
+> lastlog
+> wtmp
+> dpkg.log 
+> kern.log
+> syslog
+exit
+
--- monitor/mon_logs.py
+++ monitor/mon_logs.py
@@ -1,6 +1,128 @@
 ## Check monitor's status and exit
 import subprocess
 
+'''
+=========================== LAB RVN ===========================
+albokiadt@rig-Lab-W:~$ miners logs rvn
+==> /var/log/mining/RVN-miner-W.log <==
+[2018-05-09 07:17:42] hash order 85A78A179600515C (5af2d8d6)
+[2018-05-09 07:17:53] GPU #0: EVGA GTX 1080 Ti, 16.96 MH/s
+[2018-05-09 07:17:53] GPU #0: 1668 MHz 82.33 kH/W 222W 54C FAN 31%
+[2018-05-09 07:17:53] accepted: 613/618 (diff 0.344), 47.02 MH/s yes!
+[2018-05-09 07:17:57] GPU #1: Gigabyte GTX 1070, 46.79 MH/s
+[2018-05-09 07:17:57] GPU #1: an illegal memory access was encountered
+[2018-05-09 07:17:57] GPU #2: EVGA GTX 1080 Ti, 90.57 MH/s
+[2018-05-09 07:17:57] GPU #2: driver shutting down
+[2018-05-09 07:17:57] GPU #0: EVGA GTX 1080 Ti, 261.29 MH/s
+[2018-05-09 07:17:57] GPU #0: driver shutting down
+
+==> /var/log/mining/RVN-miner-W.err <==
+Cuda error in func 'cuda_check_cpu_setTarget' at line 41 : an illegal memory access was encountered.
+Cuda error in func 'cuda_check_cpu_setTarget' at line 41 : driver shutting down.
+Cuda error in func 'cuda_check_cpu_setTarget' at line 41 : driver shutting down.
+
+'''
+'''
+=========================== LAB RVN ===========================
+albokiadt@rig-Lab-W:~$ miners logs rvn
+==> /var/log/mining/RVN-miner-W.log <==
+==> /var/log/mining/RVN-miner-W.log <==
+[2018-05-11 03:01:00] GPU #0: EVGA GTX 1080 Ti, 14.90 MH/s
+[2018-05-11 03:01:01] accepted: 19/19 (diff 0.172), 39.60 MH/s yes!
+[2018-05-11 03:01:11] GPU #0: EVGA GTX 1080 Ti, 15.04 MH/s
+[2018-05-11 03:01:11] GPU #0: 1614 MHz 71.54 kH/W 209W 44C FAN 23%
+[2018-05-11 03:01:11] accepted: 20/20 (diff 4.116), 39.61 MH/s yes!
+[2018-05-11 03:01:34] GPU #2: EVGA GTX 1080 Ti, 15.99 MH/s
+[2018-05-11 03:01:34] accepted: 21/21 (diff 0.588), 39.60 MH/s yes!
+[2018-05-11 03:01:38] Stratum difficulty set to 17.9907 (0.07028)
+[2018-05-11 03:01:45] GPU #1: Gigabyte GTX 1070, 12.17 MH/s
+[2018-05-11 03:01:45] GPU #1: an illegal memory access was encountered
+
+==> /var/log/mining/RVN-miner-W.err <==
+Cuda error in func 'cuda_check_cpu_setTarget' at line 41 : an illegal memory access was encountered.
+'''
+'''
+albokiadt@rig-Lab-W:~$ miners logs rvn
+==> /var/log/mining/RVN-miner-W.log <==
+[2018-05-11 03:01:00] GPU #0: EVGA GTX 1080 Ti, 14.90 MH/s
+[2018-05-11 03:01:01] accepted: 19/19 (diff 0.172), 39.60 MH/s yes!
+[2018-05-11 03:01:11] GPU #0: EVGA GTX 1080 Ti, 15.04 MH/s
+[2018-05-11 03:01:11] GPU #0: 1614 MHz 71.54 kH/W 209W 44C FAN 23%
+[2018-05-11 03:01:11] accepted: 20/20 (diff 4.116), 39.61 MH/s yes!
+[2018-05-11 03:01:34] GPU #2: EVGA GTX 1080 Ti, 15.99 MH/s
+[2018-05-11 03:01:34] accepted: 21/21 (diff 0.588), 39.60 MH/s yes!
+[2018-05-11 03:01:38] Stratum difficulty set to 17.9907 (0.07028)
+[2018-05-11 03:01:45] GPU #1: Gigabyte GTX 1070, 12.17 MH/s
+[2018-05-11 03:01:45] GPU #1: an illegal memory access was encountered
+
+==> /var/log/mining/RVN-miner-W.err <==
+Cuda error in func 'cuda_check_cpu_setTarget' at line 41 : an illegal memory access was encountered.
+'''
+'''
+albokiadt@rig-Lab-W:~$ miners logs rvn
+==> /var/log/mining/RVN-miner-W.log <==
+[2018-05-11 12:41:47] GPU #2: 1715 MHz 90.14 kH/W 202W 48C FAN 0%
+[2018-05-11 12:41:47] accepted: 356/356 (diff 0.080), 44.96 MH/s yes!
+[2018-05-11 12:41:54] GPU #2: EVGA GTX 1080 Ti, 19.22 MH/s
+[2018-05-11 12:41:54] accepted: 357/357 (diff 0.463), 45.10 MH/s yes!
+[2018-05-11 12:41:58] GPU #0: EVGA GTX 1080 Ti, 17.62 MH/s
+[2018-05-11 12:41:58] hash order 4245905CDDE2CF1C (5af5c7ae)
+[2018-05-11 12:42:00] GPU #2: EVGA GTX 1080 Ti, 19.06 MH/s
+[2018-05-11 12:42:00] accepted: 358/358 (diff 0.045), 45.38 MH/s yes!
+[2018-05-11 12:42:03] GPU #1: Gigabyte GTX 1070, 20.09 MH/s
+[2018-05-11 12:42:03] GPU #1: an illegal memory access was encountered
+
+==> /var/log/mining/RVN-miner-W.err <==
+Cuda error in func 'cuda_check_cpu_setTarget' at line 41 : an illegal memory access was encountered.
+'''
+'''
+==> /var/log/mining/ORE-miner-B.log <==
+[2018-05-11 16:24:20] Stratum difficulty set to 20 (0.07812)
+[2018-05-11 16:24:20] lyra2v2 block 102656, diff 2959.544
+
+[2018-05-11 16:24:20] GPU #2: Intensity set to 20, 1048576 cuda threads
+[2018-05-11 16:24:20] GPU #3: Intensity set to 20, 1048576 cuda threads
+[2018-05-11 16:24:20] GPU #0: Intensity set to 20, 1048576 cuda threads
+[2018-05-11 16:24:20] GPU #1: Intensity set to 20, 1048576 cuda threads
+
+
+==> /var/log/mining/ORE-miner-B.err <==
+Cuda error in func 'scanhash_lyra2v2' at line 118 : invalid device ordinal.
+Cuda error in func 'scanhash_lyra2v2' at line 118 : driver shutting down.
+PLUS LOUD FAN NOISE ON GPU2!
+'''
+'''
+root@rig-BORG-A:~# miners logs
+==> /var/log/mining/RVN-miner-A.log <==
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+[2018-05-11 17:37:28] GPU #3: result for 00000000 does not validate on CPU! keccak 4E38F2D8A3EE398C
+
+==> /var/log/mining/RVN-miner-A.err <==
+Cuda error in func 'x13_hamsi512_cpu_init' at line 414 : unspecified launch failure.
+Cuda error in func 'x13_hamsi512_cpu_init' at line 414 : driver shutting down.
+'''
+'''
+FIXME:
+
+05:33:47|stratum |  Received new job 6e98a77c, from daggerhashimoto.usa.nicehash.com
+  05:33:49|cuda-6  |  Fatal GPU error: CUDA error in func search at line 504 unspecified launch failure
+  05:33:49|cuda-6  |  Terminating.
+  05:33:49|cuda-11 |  Fatal GPU error: CUDA error in func search at line 504 unspecified launch failure
+root@rig-19X:/opt/ethminer/build# 
+root@rig-19X:/opt/ethminer/build# ethminer -SP 2 -U -S daggerhashimoto.usa.nicehash.com:3353 -O 346u4oMvACtZQK9RASzcgYoZwX4JaAd3Pi.NICEHASH-ETHASH-miner:x
+
+Ctrl-C ->  '16:54:03|ethminer|  Shutting down miners...'
+'''
+
+
 ## if OPERATION=status, check monitor's status and exit
 def process(self, config, arguments):
     cmd = '/usr/bin/tail -f /var/log/mining/monitor.log'
