6c8f65aa9ed71da45d347e5411402288b420fea1
Glenn Wood <glenn@hazelst.com>
2018-06-03 13:16:33 -0400

    Better dryrun output out of 'miners swap'.

--- mining/swap.py
+++ mining/swap.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import stop
 import start
 import status
@@ -6,23 +7,31 @@ import sys
 
 def process(self, config, coin):
 
-    print "Stopping "+coin[0]['COIN']
-    stop.process(self, config, coin[0])
-
-    pinfo = status.get_status(coin[0])
-    t = 0
-    while pinfo is not None and t < 60:
-        t += 1
-        print('Waiting ... '+str(t)+' '),;sys.stdout.flush()
-        time.sleep(0.5)
-        print ('\r'),;sys.stdout.flush()
+    if config.DRYRUN:
+        stop.process(self, config, coin[0])
         pinfo = status.get_status(coin[0])
-    if t >= 60:
-        print "FAIL: Process mining "+coin[0]['COIN']+" did not stop!"
-        return 1
+        print('timeout 60 wait '+str(pinfo['pid']))
+        print('[ $? = 0 ] && ',end='')
+        start.process(self, config, coin[1])
+    else:
+        print("Stopping "+coin[0]['COIN'])
+        stop.process(self, config, coin[0])
+    
+        pinfo = status.get_status(coin[0])
+        t = 0
+        while pinfo is not None and t < 60:
+            t += 1
+            print('Waiting ... '+str(t)+' '),;sys.stdout.flush()
+            time.sleep(0.5)
+            print ('\r'),;sys.stdout.flush()
+            pinfo = status.get_status(coin[0])
+        if t >= 60:
+            print("FAIL: Process mining "+coin[0]['COIN']+" did not stop!")
+            return 1
+    
+        print("Starting "+coin[1]['COIN'])
+        start.process(self, config, coin[1])
 
-    print "Starting "+coin[1]['COIN']
-    start.process(self, config, coin[1])
     return 0
 
 def initialize(self, config, coin):
