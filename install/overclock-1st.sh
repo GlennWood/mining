621f411533414d03537b162f553cde8174914be9
Glenn Wood <glenn@hazelst.com>
2018-06-30 13:46:14 -0400

    Use latest gpustat (from github, not PyPi).

--- install/overclock-1st.sh
+++ install/overclock-1st.sh
@@ -20,6 +20,17 @@ cp /usr/share/nvidia-$NVIDIA_VER/* /usr/share/nvidia/
 mv /usr/share/nvidia/nvidia-application-profiles-${NVIDIA_VER}.${NVIDIA_SUB}-key-documentation /usr/share/nvidia/nvidia-application-profiles-key-documentation
 mv /usr/share/nvidia/nvidia-application-profiles-${NVIDIA_VER}.${NVIDIA_SUB}-rc /usr/share/nvidia/nvidia-application-profiles-rc
 nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=28 --use-display-device="DFP-0" --connected-monitor="DFP-0"
+### Having to do with message 'Xlib:  extension "GLX" missing on display ":0".',
+###   Ref: https://www.linuxquestions.org/questions/slackware-14/help-to-install-nvidia-drivers-4175546666/
+###   REMOVE ANY LINE IN /etc/X11/xorg.conf THAT CONTAINS 'Inactive "intel"' !!! 
+###   Otherwise /var/log/Xorg.0.log will show a failure at that line, and will be missing a lot of whatever process (at reboot) it is recording. weird ...
+### Regarding how xorg.conf keeps getting mangled by "something". That "something" is gpumanager!
+###   Ref: http://nicktj.tumblr.com/post/83943489732/xorgconf-reset-every-boot-in-1404-lts
+grep nogpumanager /etc/default/grub
+if [ "$?" == "1" ]; then
+  sed -i.bak 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="nogpumanager /' /etc/default/grub
+  update-grub
+fi
 
   echo "A reboot is recommended before trying any actual overclocking at this time."
   echo "Hit ctrl-C to abort the reboot."
